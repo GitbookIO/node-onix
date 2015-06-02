e106bdc6020b1fd4d55273bafae506d766f988ab
Samy Pesse <samypesse@gmail.com>
2015-06-02 20:59:34 +0200

    Add base for tests

---
+++ test/api.js
@@ -0,0 +1,12 @@
+var onix = require('../');
+var should = require('should');
+
+describe('API', function () {
+    it('should return empty xml', function() {
+        var xml = onix.create(
+
+        );
+
+
+    });
+});
