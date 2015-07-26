8a04d0c2068d0d258af1a7e3e1c721d3690d9f55
Samy Pesse <samypesse@gmail.com>
2015-07-25 23:18:36 -0700

    Add base test for parsing

---
+++ test/parse.js
@@ -0,0 +1,15 @@
+var fs = require('fs');
+var should = require('should');
+var path = require('path');
+
+var onix = require('../');
+
+describe('Parsing', function () {
+    var EPUBDIRECT = fs.readFileSync(path.join(__dirname, './fixtures/epubDirect.onx'), { encoding: 'utf-8' });
+    var feed
+
+    it('should correctly parse xml', function() {
+        feed = onix.parse(EPUBDIRECT);
+        console.log(feed);
+    });
+});
